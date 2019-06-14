78d95d6e5efa6969861403089f4a841ad69ab7c2
zcarlos <zecloureiro@gmail.com>
2019-06-14 23:43:28 +0100

    done some testing on css file, replacing translateY(-100%) by max-height: 0 and changing the over by max-height: 500px

--- menu.js
+++ menu.js
@@ -7,34 +7,27 @@ function $$(selectors) {
 }
 
 let dropdown_menu_ul_style = $$('.dropdown-menu');
-const mediaScreenWidth = window.innerWidth;
 
-window.onresize = clearStyleResizing;
-function clearStyleResizing() {
-	console.log("OUT" + $('body').clientWidth);
+window.onresize = clearStyleOnResizing;
+
+function clearStyleOnResizing() {	
 	if ($('body').clientWidth <= 625) {
-		console.log("IN" + $('body').clientWidth);
 		dropdown_menu_ul_style.forEach((item, i) => {
-			/*dropdown_menu_ul_style[i].style.transform = 'none';*/
 			dropdown_menu_ul_style[i].style.cssText = 'transform: none;';
 		})
 	} else {
 		dropdown_menu_ul_style.forEach((item, i) => {
-			/*dropdown_menu_ul_style[i].style.transform = 'translateY(-100%)';*/
 			dropdown_menu_ul_style[i].style.cssText = 'transform: translateY(-100%);';
 		})
 	}
-
 }
 
 $$('.toggle-dropdown').forEach((item, i) => {
     item.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log(mediaScreenWidth);
-        /*console.log(getComputedStyle(dropdown_menu_ul_style[i]).transform);*/
+
         if (getComputedStyle(dropdown_menu_ul_style[i]).transform === 'matrix(1, 0, 0, 1, 0, -248)') {
             clear_dropdown_menu_styles();
-            /*dropdown_menu_ul_style[i].style.transform = 'translateY(0%)';*/
             dropdown_menu_ul_style[i].style.cssText = 'transform: translateY(0%);';
         } else {
             clear_dropdown_menu_styles();
@@ -43,21 +36,18 @@ $$('.toggle-dropdown').forEach((item, i) => {
 });
 
 function clear_dropdown_menu_styles() {
-    dropdown_menu_ul_style.forEach((item, i) => {    	
-        /*dropdown_menu_ul_style[i].style.transform = 'translateY(-100%)';*/
+    dropdown_menu_ul_style.forEach((item, i) => {
         dropdown_menu_ul_style[i].style.cssText = 'transform: translateY(-100%);'; 
     })
 }
 
 /**
- * Close the dropdown when clciked outside
- * If the translated box menu is down 
+ * Close the dropdown when clicked outside if the translated box menu is down 
  */
 window.onclick = function(e) {	
 	if (!e.target.matches('.toggle-dropdown')) {
 		dropdown_menu_ul_style.forEach((item, i) => {			
             if (getComputedStyle(item).transform === 'matrix(1, 0, 0, 1, 0, 0)') {
-                /*item.style.transform = 'translateY(-100%)';*/
                 item.style.cssText = 'transform: translateY(-100%);';
             }        
         });
